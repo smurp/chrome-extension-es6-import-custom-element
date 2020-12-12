cf6f0c34f3e84e0c7d54d614cdc80831f160f927
Shawn Murphy <smurp@smurp.com>
2020-12-12 03:55:36 +0100

    document addition of customElements support

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # What is this?
 
-A working example of this Medium Article
+This project demonstrates these mutually challenging charateristics:
+
+* Google Chrome Extension
+* ... preserving access to `chrome.runtime`
+* ES6 import in the content_script
+* Custom Elements aka WebComponent, or at the very least `customElements.define()`
+
+## Credits:
+
+This repo started off as a working example of this Medium Article:
 
 [How to use ES6 “import” with Chrome Extension - Hiromu OCHIAI - Medium](https://medium.com/@otiai10/how-to-use-es6-import-with-chrome-extension-bd5217b9c978)
 
-Star this repo if you like 😉
+It was then extended by [Shawn Murphy](https://github.com/smurp) to include support for WebComponents.
+
--- src/js/content_main.js
+++ src/js/content_main.js
@@ -1,10 +1,14 @@
 import User from './models/user.js';
+import {CuStom} from './custom.js';
+
+customElements.define('cu-stom', CuStom)
 
 export function main() {
-  const user = User.new({name: 'otiai20'});
+  const user = User.new({name: 'otiai20, and this is proof that ES6 import works in content_script'});
   console.log(user.greet());
-  console.log(
-    "Is chrome.runtime available here?",
-    typeof chrome.runtime.sendMessage == "function",
-  );
+  console.log("Is chrome.runtime available here?",
+              typeof chrome.runtime.sendMessage == "function");
+  const aCustom = document.createElement('cu-stom');
+  console.log("supports custom elements: ",
+              aCustom.constructor.name == 'CuStom');
 }
